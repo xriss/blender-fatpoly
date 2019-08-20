1c8266e2851f09b9b113d2ec3aeee04009fb4004
kriss <kriss@xixs.com>
2019-08-20 05:39:55 +0100

    .

--- test2.py
+++ test2.py
@@ -1,67 +1,48 @@
 bl_info = {
-    "name": "Cursor Array",
-    "category": "Object",
+	"name": "test2",
+	"category": "Mesh",
 }
 
 import bpy
+import bmesh
 
 
-class ObjectCursorArray(bpy.types.Operator):
-    """Object Cursor Array"""
-    bl_idname = "object.cursor_array"
-    bl_label = "Cursor Array"
-    bl_options = {'REGISTER', 'UNDO'}
+class test2(bpy.types.Operator):
+	"""test2"""
+	bl_idname = "object.test2"
+	bl_label = "test2"
+	bl_options = {'REGISTER', 'UNDO'}
 
-    total: bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)
+	total: bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)
 
-    def execute(self, context):
-        scene = context.scene
-        cursor = scene.cursor.location
-        obj = context.active_object
+	def execute(self, context):
+		bm = self.get_bm(context)
+		if not bm :
+			return {'CANCELLED'}
 
-        for i in range(self.total):
-            obj_new = obj.copy()
-            scene.collection.objects.link(obj_new)
+		print(bm)
 
-            factor = i / self.total
-            obj_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
+		return {'FINISHED'}
 
-        return {'FINISHED'}
-
+	def get_bm(self,context):
+		obj = context.active_object
+		if not obj:
+			return
+		if obj.mode == "EDIT" and obj.type == "MESH":
+			return bmesh.from_edit_mesh(obj.data)
 
 def menu_func(self, context):
-    self.layout.operator(ObjectCursorArray.bl_idname)
-
-# store keymaps here to access after registration
-addon_keymaps = []
+	self.layout.operator(test2.bl_idname)
 
 
 def register():
-    bpy.utils.register_class(ObjectCursorArray)
-    bpy.types.VIEW3D_MT_object.append(menu_func)
-
-    # handle the keymap
-    wm = bpy.context.window_manager
-    # Note that in background mode (no GUI available), keyconfigs are not available either,
-    # so we have to check this to avoid nasty errors in background case.
-    kc = wm.keyconfigs.addon
-    if kc:
-        km = wm.keyconfigs.addon.keymaps.new(name='Object Mode', space_type='EMPTY')
-        kmi = km.keymap_items.new(ObjectCursorArray.bl_idname, 'T', 'PRESS', ctrl=True, shift=True)
-        kmi.properties.total = 4
-        addon_keymaps.append((km, kmi))
+	bpy.utils.register_class(test2)
+	bpy.types.VIEW3D_MT_edit_mesh.append(menu_func)
 
 def unregister():
-    # Note: when unregistering, it's usually good practice to do it in reverse order you registered.
-    # Can avoid strange issues like keymap still referring to operators already unregistered...
-    # handle the keymap
-    for km, kmi in addon_keymaps:
-        km.keymap_items.remove(kmi)
-    addon_keymaps.clear()
-
-    bpy.utils.unregister_class(ObjectCursorArray)
-    bpy.types.VIEW3D_MT_object.remove(menu_func)
+	bpy.types.VIEW3D_MT_edit_mesh.remove(menu_func)
+	bpy.utils.unregister_class(test2)
 
 
 if __name__ == "__main__":
-    register()
+	register()
