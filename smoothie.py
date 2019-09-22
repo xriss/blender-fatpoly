06bd84b7b7717c9d5e27c482c242f2f0f4470e11
kriss <kriss@xixs.com>
2019-09-23 00:31:40 +0100

    smoothie

--- smoothie.py
+++ smoothie.py
@@ -6,6 +6,8 @@ bl_info = {
 import bpy
 import bmesh
 
+from mathutils import *
+
 
 class smoothie(bpy.types.Operator):
 	"""smoothie"""
@@ -21,9 +23,47 @@ class smoothie(bpy.types.Operator):
 			return {'CANCELLED'}
 
 		print(bm)
+		
+		lens = [] # length of each edge
+		aves = [] # smoothed xyz of each vertex
 
+# initialize aves
 		for v in bm.verts:
-			v.co.x += 1.00
+			aves.append( Vector((0,0,0,0)) )
+			print(v.index,v.co)
+
+# initialize lens and build smooth aves
+		for e in bm.edges:
+			lens.append( e.calc_length() )
+			for v in e.verts:
+				aves[e.verts[0].index]+=Vector((v.co.x,v.co.y,v.co.z,1))
+				aves[e.verts[1].index]+=Vector((v.co.x,v.co.y,v.co.z,1))
+
+# apply aves to vertex location ( smooth )
+		for v in bm.verts:
+			a=aves[v.index]
+			v.co=Vector((a.x/a.w,a.y/a.w,a.z/a.w))
+			print(v.index,v.co,a)
+
+		for i in range(1,4):
+# reset aves
+			for v in bm.verts:
+				aves[v.index]=Vector((0,0,0,0))
+
+# build spring aves
+			for e in bm.edges:
+				d=Vector((e.verts[0].co-e.verts[1].co))
+				d.normalize()
+				d*=((lens[e.index]-e.calc_length())*0.5)
+				for v in e.verts:
+					aves[e.verts[0].index]+=Vector((d.x,d.y,d.z,1))
+					aves[e.verts[1].index]+=Vector((-d.x,-d.y,-d.z,1))
+
+
+# apply aves to vertex location ( springs )
+			for v in bm.verts:
+				a=aves[v.index]
+				v.co+=Vector((a.x/a.w,a.y/a.w,a.z/a.w))
 		
 		self.set_bm(context,bm)
 		
@@ -49,7 +89,7 @@ class smoothie(bpy.types.Operator):
 		if obj.type != "MESH" :
 			return
 		if obj.mode == "EDIT" :
-			bmesh.update_edit_mesh(obj.data, False, False)
+			bmesh.update_edit_mesh(obj.data, True, True)
 		else:
 			bm.to_mesh(me)
 			me.update()
@@ -69,3 +109,4 @@ def unregister():
 
 if __name__ == "__main__":
 	register()
+#	bpy.ops.object.smoothie()
