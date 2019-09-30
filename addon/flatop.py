2ed8102216844226ac43ba588f5a16a2d3cf8eea
kriss <kriss@xixs.com>
2019-09-30 04:33:24 +0100

    .

--- addon/flatop.py
+++ addon/flatop.py
@@ -80,7 +80,8 @@ class flatop(bpy.types.Operator):
 				for v in bm.verts:
 					f=floods[v.index]
 					if v.select and f<0x7fffffff and f>0 :
-						weights[v.index]+=Vector((vw.co.x,vw.co.y,vw.co.z,1))*(1.0/(f*f*f))
+						vo = vw.co
+						weights[v.index]+=Vector((vo.x,vo.y,vo.z,1))*(1.0/(f*f*f))
 
 # apply all weights to find new vertex location
 		for v in bm.verts:
@@ -89,6 +90,49 @@ class flatop(bpy.types.Operator):
 				if a.w!=0 :
 					v.co=Vector((a.x,a.y,a.z))/a.w
 
+		bm.normal_update()
+
+# init
+		for vw in bm.verts:
+			weights[vw.index]=Vector((0,0,0,0))
+
+# process each vertex
+		for vw in bm.verts:
+# this vertex is locked
+			if not vw.select :
+# reset flood distance calc for this vertex
+				for v in bm.verts:
+					floods[v.index]=0x7fffffff
+# seed start of flood
+				floods[vw.index]=1.0
+				active=True
+				while active :
+					active=False
+					for e in bm.edges:
+						for ia in range(2): # flip flop
+							va=e.verts[ia]
+							vb=e.verts[(ia+1)%2]
+							if vb.select and floods[vb.index] > floods[va.index]+1 :
+								floods[vb.index] = floods[va.index]+1
+								active=True
+# add weight dependent on edge distance from main vertex
+				for v in bm.verts:
+					f=floods[v.index]
+					if v.select and f<0x7fffffff and f>0 :
+						vo = vw.co + vw.normal*(v.co-vw.co).length*f/2
+						weights[v.index]+=Vector((vo.x,vo.y,vo.z,1))*(1.0/(f*f*f))
+
+# apply all weights to find new vertex location
+		for v in bm.verts:
+			a=weights[v.index]
+			if v.select :
+				if a.w!=0 :
+					v.co=Vector((a.x,a.y,a.z))/a.w
+
+		bm.normal_update()
+		self.set_bm(context,bm)
+		
+		return {'FINISHED'}
 
 
 		bm.normal_update()
