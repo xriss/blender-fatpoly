4b555c7634f4fa26ebb38a66fac771e805108466
kriss <kriss@xixs.com>
2019-10-09 16:42:30 +0100

    .

--- python/fatpoly.py
+++ python/fatpoly.py
@@ -6,14 +6,14 @@ base_path = Path(__file__).parent
 
 print( "LOADING FATPOLY" )
 
-for name in ("fatpoly_smoothie.py") :
+for name in ( "fatpoly_smoothie.py" , "" ) :
+	if name :
+		filepath=(base_path / name).resolve()
+		print( filepath )
 
-	filepath=(base_path / name).resolve()
-	print( filepath )
-
-	global_namespace = {"__file__": filepath, "__name__": "__main__"}
-	with open(filepath, 'rb') as file:
-		exec(compile(file.read(), filepath, 'exec'), global_namespace)
-    
+		global_namespace = {"__file__": filepath, "__name__": "__main__"}
+		with open(filepath, 'rb') as file:
+			exec(compile(file.read(), filepath, 'exec'), global_namespace)
+		
     
 print( "LOADED FATPOLY" )
